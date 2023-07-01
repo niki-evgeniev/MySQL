90770f6568e9cf4687e56067d6d0b78fc663dfac
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-07-02 02:14:36 +0300

    asd

---
+++ Exercise Functions and Procedures/02. Employees with Salary Above Number.sql
@@ -0,0 +1,11 @@
+DELIMITER //
+CREATE PROCEDURE usp_get_employees_salary_above(more_that_salary DECIMAL(20, 4))
+BEGIN
+    SELECT first_name, last_name
+    FROM employees
+    WHERE salary >= more_that_salary
+    ORDER BY first_name, last_name, employee_id;
+
+end //
+
+CALL usp_get_employees_salary_above(45000);
