6ce261e59a446dbb907a4a4d9ed1c0bea96e67ea
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-07-05 01:32:28 +0300

    Create 07. Define Function.sql

---
+++ Exercise Functions and Procedures/07. Define Function.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50))
+    RETURNS BIT
+    RETURN word REGEXP (concat('^[', set_of_letters, ']+$'));
+
+SELECT ufn_is_word_comprised('oistmiahf', 'Sofia');
+SELECT ufn_is_word_comprised('oistmiahf', 'halves');
+SELECT ufn_is_word_comprised('bobr', 'Rob');
+SELECT ufn_is_word_comprised('pppp', 'Guy');
+
+#---------------------------------------------
+
+CREATE FUNCTION ufn_is_word_comprised2(set_of_letters varchar(50), word varchar(50))
+    RETURNS BIT
+    RETURN word LIKE CONCAT('%', set_of_letters, '%');
+
+SELECT ufn_is_word_comprised2('oistmiahf', 'Sofia');
+SELECT ufn_is_word_comprised2('oistmiahf', 'halves');
