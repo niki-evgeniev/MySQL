1d9ecae83273acea23f5e1b689847e5d3066ac83
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-07-19 01:27:33 +0300

    Create 5. Online Store Database.sql

---
+++ Exercises Table Relations/5. Online Store Database.sql
@@ -0,0 +1,54 @@
+CREATE TABLE item_types
+(
+    item_type_id INT PRIMARY KEY AUTO_INCREMENT,
+    name         VARCHAR(50)
+);
+
+CREATE TABLE items
+(
+    item_id      INT PRIMARY KEY AUTO_INCREMENT,
+    name         VARCHAR(50),
+    item_type_id INT,
+    CONSTRAINT fk_items_item_types
+        FOREIGN KEY (item_type_id)
+            REFERENCES item_types (item_type_id)
+
+);
+CREATE TABLE cities
+(
+    city_id INT PRIMARY KEY AUTO_INCREMENT,
+    name    VARCHAR(50)
+);
+
+CREATE TABLE customers
+(
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(50),
+    birthday    DATE,
+    city_id     INT,
+    CONSTRAINT fk_customers_cities
+        FOREIGN KEY (city_id)
+            REFERENCES cities (city_id)
+);
+
+CREATE TABLE orders
+(
+    order_id    INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    CONSTRAINT fk_orders_customers
+        FOREIGN KEY (customer_id)
+            REFERENCES customers (customer_id)
+);
+
+CREATE TABLE order_items
+(
+    order_id INT,
+    item_id  INT,
+    CONSTRAINT pk_order_item PRIMARY KEY (order_id, item_id),
+    CONSTRAINT fk_order_items__items
+        FOREIGN KEY (item_id)
+            REFERENCES items (item_id),
+    CONSTRAINT fk_oreder_items__orders
+        FOREIGN KEY (order_id)
+            REFERENCES orders (order_id)
+);
